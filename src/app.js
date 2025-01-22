2ebaac41279526c9e30e28ee4c28ea2d53f0583b
uday919 <udaykiran919p@gmail.com>
2025-01-21 19:30:38 -0600

    Auth

--- src/app.js
+++ src/app.js
@@ -1,18 +1,74 @@
-const { auth } = require('./auth');
+const { auth } = require('./middlewares/auth');
 const express = require('express');
 const connectDB = require('./config/database');
 const User = require('./models/user');
+const { validateSignUpData } = require('./utils/validation');
+const bcrypt = require('bcrypt');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.post('/signup', async (req, res) => {
-  const user = new User(req.body);
   try {
+    validateSignUpData(req);
+    const { firstName, lastName, emailId, password } = req.body;
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: passwordHash,
+    });
+
     await user.save();
     res.send('user added successfully');
   } catch (err) {
-    res.status(400).send('Error saving the user');
+    res.status(400).send('Error :' + err.message);
   }
+});
+
+app.post('/login', async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
+    const user = await User.findOne({ emailId: emailId });
+    if (!user) {
+      throw new Error('Invalid credentials');
+    }
+
+    const isPasswordValid = await user.validatePassword(password);
+    if (isPasswordValid) {
+      const token = await user.getJWT();
+
+      res.cookie('token', token, {
+        expires: new Date(Date.now() + 8 * 3600000),
+      });
+
+      res.send('login successful');
+    } else {
+      throw new Error('invalid credentials');
+    }
+  } catch (err) {
+    res.status(400).send('Error :' + err.message);
+  }
+});
+
+app.get('/profile', auth, async (req, res) => {
+  try {
+    const user = req.user;
+
+    res.send(user);
+  } catch (err) {
+    res.status(400).send('ERROR :' + err.message);
+  }
+});
+
+app.post('/sendConnectionRequest', auth, async (req, res) => {
+  const user = req.user;
+  console.log('sending a connection request');
+  res.send(user.firstName + 'sent the connection request');
 });
 
 app.get('/user', async (req, res) => {
--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const useSchema = new mongoose.Schema(
   {
     firstName: {
@@ -16,9 +19,20 @@ const useSchema = new mongoose.Schema(
       lowercase: true,
       unique: true,
       trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new error('Invalid email address' + value);
+        }
+      },
     },
     password: {
       type: String,
+      required: true,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new error('Enter a strong password' + value);
+        }
+      },
     },
     age: {
       type: Number,
@@ -36,6 +50,11 @@ const useSchema = new mongoose.Schema(
       type: String,
       default:
         'https://cdn.vectorstock.com/i/500p/11/69/blank-avatar-profile-picture-vector-45161169.jpg',
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new error('Invalid photo url' + value);
+        }
+      },
     },
     about: {
       type: String,
@@ -43,11 +62,28 @@ const useSchema = new mongoose.Schema(
     },
     skills: {
       type: [String],
+      minLength: 5,
     },
   },
   {
     timestamps: true,
   }
 );
+useSchema.methods.getJWT = async function () {
+  const user = this;
+  const token = await jwt.sign({ _id: user._id }, 'DEV@Tinder', {
+    expiresIn: '7d',
+  });
+  return token;
+};
+useSchema.methods.validatePassword = async function (passwordInputByUser) {
+  const user = this;
+  const passwordHash = user.password;
+  const isPasswordValid = await bcrypt.compare(
+    passwordInputByUser,
+    passwordHash
+  );
+  return isPasswordValid;
+};
 
 module.exports = mongoose.model('User', useSchema);
